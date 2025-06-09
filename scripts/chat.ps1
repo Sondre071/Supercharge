57c1a3ed13d2cedb540122118ba6778634479078
Sondre071 <Sirciller@gmail.com>
2025-06-09 22:31:19 +0200

    Further expand chat script

--- scripts/chat.ps1
+++ scripts/chat.ps1
@@ -3,47 +3,89 @@ param(
 )
 if (-not $ProjectRoot) { throw "project root path not provided." }
 
-$messageHistory = @()
+function Open-MainMenu() {
+    $options = ('New chat', 'Prompts', 'Models')
 
-Write-MenuHeader -Header "Chat session"
+    $action = Read-Menu -Header 'Chat' -Options $options -ExitOption 'Exit'
 
-while ($true) {
-    $processInfo = [System.Diagnostics.ProcessStartInfo]::new()
-    $processInfo.FileName = "$ProjectRoot/bin/chat.exe"
-    $processInfo.RedirectStandardOutput = $true
-    $processInfo.UseShellExecute = $false
-    $processInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    switch ($action) {
 
-    $userInput = Read-Host "`nYou"
+        'New chat' { New-Chat }
 
-    if ($userInput -eq "") { break }
+        'Prompts' { Open-PromptsMenu }
 
-    $messageHistory += @{ role = "user"; content = $userInput }
+        'Models' { Open-ModelsMenu }
 
-    $jsonToSend = ` $messageHistory | ConvertTo-Json `
-        -AsArray `
-        -Compress `
-        -Depth 10
-
-    $base64Json = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($jsonToSend))
-
-    $processInfo.Arguments = "`"$ProjectRoot`" `"$base64Json`""
-    $process = [System.Diagnostics.Process]::Start($processInfo)
-    ##process.StandardInput.WriteLine($jsonToSend)
-    #$process.StandardInput.Close()
-
-    $modelResponse = ""
-
-    Write-Host 
-    while ($null -ne ($jsonBytes = $process.StandardOutput.ReadLine())) {
-        $responseChunk = $jsonBytes | ConvertFrom-Json
-
-        Write-Host $responseChunk -ForegroundColor Yellow -NoNewLine
-        $modelResponse += $responseChunk
+        'Exit' { return }
     }
-    Write-Host 
-
-    $messageHistory += @{
-        role = "assistant"; content = $modelResponse
-    } 
 }
+
+function New-Chat() {
+    $messageHistory = @()
+
+    $availablePrompts = Get-ChildItem -Path "$ProjectRoot/data/prompts" | ForEach-Object { $_.BaseName }
+
+    if ($availablePrompts.Length -gt 0) {
+
+        $selectedPrompt = Read-Menu -Header 'Select prompt' -Options $availablePrompts -ExitOption 'Exit'
+
+        switch ($selectedPrompt) {
+            'Exit' { return }
+
+            default { 
+                $promptData = Get-Content "$ProjectRoot/data/prompts/$selectedPrompt.txt"
+        
+                $messageHistory += @{
+                    role = "system"; content = $promptData
+                }
+            }
+        }
+    }
+
+    Write-MenuHeader -Header "Chat session"
+
+    while ($true) {
+        $processInfo = [System.Diagnostics.ProcessStartInfo]::new()
+        $processInfo.FileName = "$ProjectRoot/bin/chat.exe"
+        $processInfo.RedirectStandardOutput = $true
+        $processInfo.UseShellExecute = $false
+        $processInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+
+        $userInput = Read-Host "`nYou"
+
+        if ($userInput -eq "") { break }
+
+        $messageHistory += @{
+            role = "user"; content = $userInput
+        }
+
+        $jsonToSend = $messageHistory | ConvertTo-Json `
+            -AsArray `
+            -Compress `
+            -Depth 10
+
+        $base64Json = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($jsonToSend))
+
+        $processInfo.Arguments = "`"$ProjectRoot`" `"$base64Json`""
+        $process = [System.Diagnostics.Process]::Start($processInfo)
+        ##process.StandardInput.WriteLine($jsonToSend)
+        #$process.StandardInput.Close()
+
+        $modelResponse = ""
+
+        Write-Host 
+        while ($null -ne ($jsonBytes = $process.StandardOutput.ReadLine())) {
+            $responseChunk = $jsonBytes | ConvertFrom-Json
+
+            Write-Host $responseChunk -ForegroundColor Cyan -NoNewLine
+            $modelResponse += $responseChunk
+        }
+        Write-Host 
+
+        $messageHistory += @{
+            role = "assistant"; content = $modelResponse
+        } 
+    }
+}
+
+Open-MainMenu

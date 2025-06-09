4850223d975c3e330238965422ccc9e47be3c45c
Sondre071 <Sirciller@gmail.com>
2025-06-09 22:59:08 +0200

    Formatting

--- Supercharge.psm1
+++ Supercharge.psm1
@@ -1,14 +1,16 @@
 Import-Module "$PSScriptRoot/scripts/helpers/Read-Menu.psm1"
 
-# Recompile each time.
+# Recompile on every boot.
 $devMode = $true
 
 function SU() {
-    if ($devMode -or (Get-ChildItem -Path "./bin" -Filter *.exe) -lt 1) { . "$PSScriptRoot/scripts/helpers/build.ps1" -ProjectRoot $PSScriptRoot }
+    if ($devMode -or (Get-ChildItem -Path "./bin" -Filter *.exe) -lt 1) {
+        . "$PSScriptRoot/scripts/helpers/build.ps1" -ProjectRoot $PSScriptRoot
+    }
 
     $scriptFiles = Get-ChildItem -Path "$PSScriptRoot/scripts" -File -Filter *.ps1 | Sort-Object Name
 
-    $options = $scriptFiles | ForEach-Object { $_.BaseName.Substring(0,1).ToUpper() + $_.BaseName.Substring(1) }
+    $options = $scriptFiles | ForEach-Object { $_.BaseName.Substring(0, 1).ToUpper() + $_.BaseName.Substring(1) }
 
     $action = Read-Menu -Options $options -Header "Supercharge071" -ExitOption 'Exit'
 

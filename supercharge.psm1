ecb740c52ddc83722efb0e85966e3615c52771c7
Sondre071 <Sirciller@gmail.com>
2025-08-31 01:08:54 +0200

    Improve entry script

--- supercharge.psm1
+++ supercharge.psm1
@@ -1,7 +1,33 @@
-function SU() {
-    go build -o "$PSScriptRoot/bin/supercharge.exe" "$PSScriptRoot/cmd/supercharge/main.go"
+$devMode = $true
 
-    & "$PSScriptRoot/bin/supercharge.exe"
+function SU() {
+
+    try {
+
+        if ($devMode) {
+
+            Build-Project
+        }
+        else {
+            if (-not (Test-Path -Path "$PSScriptRoot/bin/supercharge.exe")) {
+                Build-Project
+            }
+        }
+
+        & "$PSScriptRoot/bin/supercharge.exe"
+
+    }
+    catch {
+        Write-Host $_ -ForegroundColor Red
+    }
+}
+
+function Build-Project() {
+            Write-Host "Building..." -NoNewLine -ForegroundColor DarkGray
+
+            go build -o "$PSScriptRoot/bin/supercharge.exe" "$PSScriptRoot/cmd/supercharge/main.go"
+
+            Write-Host " Done!`n" -ForegroundColor DarkGray
 }
 
 Export-ModuleMember -Function SU

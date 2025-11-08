3b7e0e10643a710b3800b8f55596f099dc47b905
Sondre071 <Sirciller@gmail.com>
2025-11-08 16:20:42 +0100

    Change project structure

--- supercharge.psm1
+++ supercharge.psm1
@@ -1,16 +1,49 @@
-$devMode = $true
+$ProjectRoot = $PSScriptRoot
 
 function su($Command) {
 
-    if ($true -eq $devMode) {
-        Write-Host "Building..." -NoNewLine -ForegroundColor DarkGray
+    Confirm-LocalFiles
+
+    [psobject[]]$options = @()
+
+    $options += Get-ChildItem -Path (Join-Path $ProjectRoot 'scripts') -File | Select-Object -ExpandProperty BaseName
+
+    $choice = Read-Menu -Options ($options) -ExitOption 'Exit'
+
+    if ($choice -eq 'Exit') { return }
     
-        go build -o "$PSScriptRoot/bin/files.exe" "$PSScriptRoot/internal/files/files.go"
-    
-        Write-Host " Done!`n" -ForegroundColor DarkGray
+    . (Join-Path "$ProjectRoot/scripts" $choice) -ProjectRoot $ProjectRoot
+}
+
+function Confirm-LocalFiles() {
+    $path = Join-Path $ProjectRoot 'data'
+
+    if (-not (Test-Path $path)) {
+        New-Item -Path $path -ItemType Directory
     }
 
-    . $PSScriptRoot\Scripts\Menu\HomeMenu.ps1 -ProjectRoot $PSScriptRoot
+    if (-not (Test-Path (Join-Path $path 'config.json'))) {
+        $configContent = [psobject]@{
+            OpenRouter = [psobject]@{
+                ApiKey        = ''
+                ApiUrll       = ''
+                CurrentModel  = ''
+                CurrentPreset = ''
+            }
+        }
+
+        $jsonContent = $configContent | ConvertTo-Json -Depth 3
+
+        New-Item -Path (Join-Path $path 'config.json') -ItemType File -Value $jsonContent
+    }
+
+    $filesToCopy = Get-ChildItem `
+        -Path (Join-Path $ProjectRoot 'build_scripts')
+
+    foreach ($file in $filesToCopy) {
+        Copy-Item -Path $file.FullName -Destination "$ProjectRoot/scripts/$($file.Name)"
+        Write-Host "Copied $($file.Name)"
+    }
 }
 
 Export-ModuleMember -Function su

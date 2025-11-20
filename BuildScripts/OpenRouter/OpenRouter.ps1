23cee074a015a98aec6f8804fd6540855e921f7a
Sondre071 <sirciller@gmail.com>
2025-11-20 18:26:26 +0100

    Refactor scripts into functions

--- BuildScripts/OpenRouter/OpenRouter.ps1
+++ BuildScripts/OpenRouter/OpenRouter.ps1
@@ -3,21 +3,21 @@ param (
 )
 
 $HelpersPath = Join-Path $ProjectRoot 'Scripts' 'Helpers' 'OpenRouter'
-$newChatScript = Join-Path $HelpersPath 'NewChat.ps1'
 
-###
+. (Join-Path $HelpersPath 'New-Chat.ps1') -HelpersPath $HelpersPath
 
 $configPath = Join-Path $env:UserProfile '.supercharge' 'openrouter.json'
 $config = PSModuleManager `
     -FilePath $configPath `
     -InitialJSONContent '{"ApiKey":"","Url":"https://openrouter.ai/api/v1/responses","CurrentModel":"",}'
 
+###
+
 $choice = Read-Menu -Header 'OpenRouter' -Options ('New chat')
 
 switch ($choice) {
     'New chat' {
-        & $newChatScript `
-            -Config $config `
-            -HelpersPath $HelpersPath
+        New-Chat `
+        -Config $config `
     }
 }

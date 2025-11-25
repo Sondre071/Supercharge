ccf61515ae86253e94c8c5b5407d395533269210
Sondre071 <sirciller@gmail.com>
2025-11-25 21:10:15 +0100

    Reorganize scripts

--- Supercharge.psm1
+++ Supercharge.psm1
@@ -3,8 +3,6 @@ Set-StrictMode -Version Latest
 $ProjectRoot = $PSScriptRoot
 
 function su() {
-    Confirm-LocalFiles
-
     $options = Get-ChildItem `
         -Path (Join-Path $ProjectRoot 'Scripts') `
         -File
@@ -25,57 +23,6 @@ function su() {
         if ($choice -eq 'Exit') { return }
 
         . $choice.Path -ProjectRoot $ProjectRoot
-    }
-}
-
-function Confirm-LocalFiles() {
-    $scripts = Get-ChildItem `
-        -Path (Join-Path $ProjectRoot 'BuildScripts') `
-        -File
-
-    if (-not (Test-Path (Join-Path $ProjectRoot 'Scripts'))) {
-        New-Item `
-            -Path (Join-Path $ProjectRoot 'Scripts') `
-            -ItemType Directory `
-        | Out-Null
-    }
-
-    foreach ($script in $scripts) {
-        $destination = Join-Path $ProjectRoot 'Scripts' $script.Name
-
-        Copy-Item `
-            -Path $script.FullName `
-            -Destination $destination `
-            -Force
-    }
-
-    $scriptHelpers = Get-ChildItem `
-        -Path (Join-Path $ProjectRoot 'BuildScripts' 'Helpers') `
-        -File `
-        -Recurse
-
-    if (-not (Test-Path (Join-Path $ProjectRoot 'Scripts' 'Helpers'))) {
-        New-Item `
-            -Path (Join-Path $ProjectRoot 'Scripts' 'Helpers') `
-            -ItemType Directory `
-        | Out-Null
-    }
-
-
-    foreach ($script in $scriptHelpers) {
-        if (-not (Test-Path (Join-Path $ProjectRoot 'Scripts' 'Helpers' $script.Directory.Name))) {
-            New-Item `
-                -Path (Join-Path $ProjectRoot 'Scripts' 'Helpers' $script.Directory.Name) `
-                -ItemType Directory `
-            | Out-Null
-        }
-
-        $destination = Join-Path $ProjectRoot 'Scripts' 'Helpers' $script.Directory.Name $script.Name
-
-        Copy-Item `
-            -Path $script.FullName `
-            -Destination $destination `
-            -Force
     }
 }
 

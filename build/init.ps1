57c5f0ca9b9c12a2ac9b2700dea7355b76727323
Sondre071 <sirciller@gmail.com>
2025-12-10 11:44:49 +0100

    Update init script

--- build/init.ps1
+++ build/init.ps1
@@ -1,27 +1,33 @@
 $dataPath = Join-Path $env:UserProfile '.supercharge' 'data'
-$jsonPath = Join-Path $env:UserProfile '.supercharge' 'data' 'openrouter.json'
-$promptsPath = Join-Path $env:UserProfile '.supercharge' 'data' 'prompts'
+$openrouterDataPath = Join-Path $dataPath 'openrouter.json'
+$blobstorageDataPath = Join-Path $dataPath 'blobstorage.json'
 
 if (-not (Test-Path $dataPath)) {
     New-Item -ItemType Directory -Path $dataPath
 }
 
-if (-not (Test-Path $jsonPath)) {
+if (Test-Path $openRouterDataPath) {
+    Write-Host "openrouter.json already exists, skipping this step.." -ForegroundColor DarkGreen
+}
+else {
     $initialContent = @{
-        apiKey     = ""
-        model      = ""
-        models     = @()
+        api_key    = ""
+        model      = "anthropic/claude-sonnet-4.5"
+        models     = @(
+            "anthropic/claude-haiku-4.5",
+            "anthropic/claude-sonnet-4.5",
+            "openai/gpt-5.1"
+        )
         parameters = @{
-            temperature        = 1.0
-            top_p              = 1.0
+            temperature        = 0.6
+            top_p              = 0.2
             top_k              = 0
             frequency_penalty  = 0.0
             presence_penalty   = 0.0
-            repetition_penalty = 0.0
+            repetition_penalty = - 1.0
             min_p              = 0.0
             top_a              = 0.0
         }
-        prompts    = $promptsPath
     }
 
     $json = $initialContent | ConvertTo-Json `
@@ -29,6 +35,29 @@ if (-not (Test-Path $jsonPath)) {
         -Compress
 
     New-Item `
-        -Path $jsonPath
-    -Value $json
+        -Path $openrouterDataPath `
+        -Value $json
+}
+
+if (Test-Path $blobstorageDataPath) {
+    Write-Host "blobstorage.json already exists, skipping this step.." -ForegroundColor DarkGreen
+}
+else {
+    $initialContent = @{
+        storage_accounts = @(
+            @{
+                name              = ""
+                local_files_path  = ""
+                connection_string = ""
+            }
+        )
+    }
+
+    $json = $initialContent | ConvertTo-Json `
+        -Depth 7 `
+        -Compress
+
+    New-Item `
+        -Path $blobstorageDataPath `
+        -Value $json
 }

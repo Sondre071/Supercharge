50bffe10d7cdd2063ba1c31b1f888744c992ea5d
Sondre071 <sirciller@gmail.com>
2026-01-03 17:58:14 +0100

    Filter out non-file prompts

--- src/openrouter/utils/get_prompts.rs
+++ src/openrouter/utils/get_prompts.rs
@@ -7,8 +7,12 @@ pub fn get_prompts() -> Vec<PromptFile> {
         std::fs::read_dir(statics::prompts_dir()).expect("Failed to read from prompts folder.");
 
     let prompts: Vec<PromptFile> = entries
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| {
+            let file_type = entry.file_type().unwrap();
+            file_type.is_file()
+        })
         .filter_map(|file| {
-            let file = file.expect("Failed to parse file.");
             let file_name = file.file_name().to_str().unwrap().to_string();
             let file_path = file.path();
 

fbc04f1be1aac24394a1dbb3976e23f5830b402c
Sondre071 <sirciller@gmail.com>
2026-02-17 22:16:55 +0100

    Make menu rendering use a vector

--- src/shared/menu/cursor.rs
+++ src/shared/menu/cursor.rs
@@ -6,9 +6,13 @@ pub struct Cursor<'a> {
 
     pub items: Vec<&'a str>,
     pub current: usize,
-    offset: usize,
     pub visible_items: usize,
     pub total_height: usize,
+
+    offset: usize,
+    //submenu_active: bool,
+    //submenu_current: usize,
+    //submenu_x_offset: usize,
 }
 
 impl<'a> Cursor<'a> {
@@ -16,6 +20,8 @@ impl<'a> Cursor<'a> {
         let subheaders = subheaders.unwrap_or(vec![]);
         let visible_items = items.len().min(20);
         let total_height = 1 + subheaders.len() + visible_items;
+
+        //let items_max_length = items.iter().map(|i| i.len()).max().unwrap();
 
         Self {
             header,
@@ -25,30 +31,44 @@ impl<'a> Cursor<'a> {
             offset: 0,
             visible_items,
             total_height,
+            //submenu_active: false,
+            //submenu_current: 0,
+            //submenu_x_offset: items_max_length + 3
         }
     }
 
     pub fn render_menu(&self) {
+        let mut lines: Vec<String> = Vec::new();
+
         let height = self.items.len().min(20);
         let offset = self.offset;
 
         for i in offset..(height + offset) {
-            if i == self.current {
-                println!(
-                    "{clear_line}{yellow}> {}{reset}",
-                    self.items[i],
-                    clear_line = ACTIONS.ClearLine,
-                    yellow = COLORS.Yellow,
-                    reset = COLORS.Reset
-                );
-            } else {
-                println!(
-                    "{clear_line}  {}{reset}",
-                    self.items[i],
-                    clear_line = ACTIONS.ClearLine,
-                    reset = COLORS.Reset
-                );
-            }
+            
+            let line = {
+                if i == self.current {
+                    format!(
+                        "{clear_line}{yellow}> {}{reset}",
+                        self.items[i],
+                        clear_line = ACTIONS.ClearLine,
+                        yellow = COLORS.Yellow,
+                        reset = COLORS.Reset
+                    )
+                } else {
+                    format!(
+                        "{clear_line}  {}{reset}",
+                        self.items[i],
+                        clear_line = ACTIONS.ClearLine,
+                        reset = COLORS.Reset
+                    )
+                }
+            };
+
+            lines.push(line);
+        }
+        
+        for line in lines {
+            println!("{}", line);
         }
     }
 

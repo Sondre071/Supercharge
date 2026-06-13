6638696a5a1f036fbe3d2cca2f6537b94075431b
Sondre071 <sirciller@gmail.com>
2026-06-13 16:18:20 +0200

    Remove unused compile attribute

--- src/shared/terminal/ansi.rs
+++ src/shared/terminal/ansi.rs
@@ -1,4 +1,4 @@
-#[allow(non_snake_case, dead_code)]
+#[allow(dead_code)]
 pub mod codes {
     pub const BORDER_COLOR: &str = "\x1b[38;2;238;150;75m";
     pub const INFO_COLOR: &str = "\x1b[38;2;128;193;254m";
@@ -12,11 +12,11 @@ pub mod codes {
     pub const SUCCESS_COLOR: &str = "\x1b[1;32m";
     pub const WARNING_COLOR: &str = "\x1b[38;2;254;225;120m";
     pub const DANGER_COLOR: &str = "\x1b[1;31m";
-    
+
     pub const RESET_COLOR: &str = "\x1b[0m";
-    
+
     pub const CLEAR_LINE: &str = "\x1b[2K";
     pub const CLEAR_SCREEN: &str = "\x1b[2J";
     pub const CLEAR_TO_END: &str = "\x1b[J";
-    pub const RESET_LOCATION: &str = "\x1b[H";
+    pub const RESET_CURSOR_LOCATION: &str = "\x1b[H";
 }
